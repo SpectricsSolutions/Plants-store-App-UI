dbe05cf070e6ea53854f5c9fb1e58217db8bb6cd
Spectrics Solutions <spectricssolutions@gmail.com>
2020-05-02 16:33:19 +0530

    Code Upload

---
+++ _Android_first_screen_1588417144032/java/exportkit/xd/first_screen_activity.java
@@ -0,0 +1,116 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		first_screen
+	 *	@date 		0
+	 *	@title 		First Screen
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.2.8.xd
+	 *
+	 */
+	
+
+package exportkit.xd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class first_screen_activity extends Activity {
+
+	
+	private View _bg__first_screen;
+	private ImageView path_1;
+	private View ellipse_5;
+	private View ellipse_6;
+	private View ellipse_2;
+	private View ellipse_3;
+	private View ellipse_4;
+	private ImageView icon_ionic_ios_arrow_back;
+	private TextView our_plants;
+	private TextView explore_more;
+	private TextView lorem_ipsum_dammy_plants;
+	private TextView price___66___________size__medium_____________plant__gimam___;
+	private ImageView _3522;
+	private ImageView icon_ionic_ios_arrow_round_back;
+	private ImageView icon_ionic_ios_arrow_round_back_ek1;
+	private View rectangle_2;
+	private ImageView icon_map_search;
+	private TextView search__;
+	private View rectangle_3;
+	private ImageView _3767;
+	private TextView gbit_plant;
+	private TextView __88;
+	private View rectangle_3_ek1;
+	private TextView gbit_plant_ek1;
+	private TextView __88_ek1;
+	private View rectangle_3_ek2;
+	private ImageView _3767_ek1;
+	private TextView gbit_plant_ek2;
+	private ImageView _3592;
+	private ImageView full_size;
+	private ImageView icon_ionic_ios_heart_empty;
+	private ImageView shopping_cart;
+	private ImageView home;
+	private ImageView full_size_ek1;
+	private ImageView mask_group_1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.first_screen);
+
+		
+		_bg__first_screen = (View) findViewById(R.id._bg__first_screen);
+		path_1 = (ImageView) findViewById(R.id.path_1);
+		ellipse_5 = (View) findViewById(R.id.ellipse_5);
+		ellipse_6 = (View) findViewById(R.id.ellipse_6);
+		ellipse_2 = (View) findViewById(R.id.ellipse_2);
+		ellipse_3 = (View) findViewById(R.id.ellipse_3);
+		ellipse_4 = (View) findViewById(R.id.ellipse_4);
+		icon_ionic_ios_arrow_back = (ImageView) findViewById(R.id.icon_ionic_ios_arrow_back);
+		our_plants = (TextView) findViewById(R.id.our_plants);
+		explore_more = (TextView) findViewById(R.id.explore_more);
+		lorem_ipsum_dammy_plants = (TextView) findViewById(R.id.lorem_ipsum_dammy_plants);
+		price___66___________size__medium_____________plant__gimam___ = (TextView) findViewById(R.id.price___66___________size__medium_____________plant__gimam___);
+		_3522 = (ImageView) findViewById(R.id._3522);
+		icon_ionic_ios_arrow_round_back = (ImageView) findViewById(R.id.icon_ionic_ios_arrow_round_back);
+		icon_ionic_ios_arrow_round_back_ek1 = (ImageView) findViewById(R.id.icon_ionic_ios_arrow_round_back_ek1);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		icon_map_search = (ImageView) findViewById(R.id.icon_map_search);
+		search__ = (TextView) findViewById(R.id.search__);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		_3767 = (ImageView) findViewById(R.id._3767);
+		gbit_plant = (TextView) findViewById(R.id.gbit_plant);
+		__88 = (TextView) findViewById(R.id.__88);
+		rectangle_3_ek1 = (View) findViewById(R.id.rectangle_3_ek1);
+		gbit_plant_ek1 = (TextView) findViewById(R.id.gbit_plant_ek1);
+		__88_ek1 = (TextView) findViewById(R.id.__88_ek1);
+		rectangle_3_ek2 = (View) findViewById(R.id.rectangle_3_ek2);
+		_3767_ek1 = (ImageView) findViewById(R.id._3767_ek1);
+		gbit_plant_ek2 = (TextView) findViewById(R.id.gbit_plant_ek2);
+		_3592 = (ImageView) findViewById(R.id._3592);
+		full_size = (ImageView) findViewById(R.id.full_size);
+		icon_ionic_ios_heart_empty = (ImageView) findViewById(R.id.icon_ionic_ios_heart_empty);
+		shopping_cart = (ImageView) findViewById(R.id.shopping_cart);
+		home = (ImageView) findViewById(R.id.home);
+		full_size_ek1 = (ImageView) findViewById(R.id.full_size_ek1);
+		mask_group_1 = (ImageView) findViewById(R.id.mask_group_1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ _Android_first_screen___2_1588417315850/java/exportkit/xd/first_screen___2_activity.java
@@ -0,0 +1,114 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		first_screen___2
+	 *	@date 		0
+	 *	@title 		First Screen  2
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.2.8.xd
+	 *
+	 */
+	
+
+package exportkit.xd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class first_screen___2_activity extends Activity {
+
+	
+	private View _bg__first_screen___2;
+	private ImageView path_1;
+	private View ellipse_2;
+	private View ellipse_3;
+	private View ellipse_4;
+	private ImageView icon_ionic_ios_arrow_back;
+	private View full_size;
+	private View full_size_ek1;
+	private ImageView icon_ionic_ios_heart_empty;
+	private ImageView shopping_cart;
+	private ImageView home;
+	private ImageView full_size_ek2;
+	private ImageView mask_group_1;
+	private TextView details;
+	private TextView reviews;
+	private TextView family;
+	private TextView size;
+	private TextView cetagory;
+	private TextView gimbarg;
+	private TextView height__62_inc;
+	private TextView indor;
+	private ImageView icon_awesome_star;
+	private ImageView icon_awesome_star_ek1;
+	private ImageView icon_awesome_star_ek2;
+	private ImageView icon_awesome_star_ek3;
+	private ImageView icon_awesome_star_ek4;
+	private TextView details_ek1;
+	private View ellipse_7;
+	private ImageView _3606;
+	private View rectangle_4;
+	private TextView price___200;
+	private TextView buy_it_now;
+	private TextView lorem_ipsum_is_simply_dummy_text_of_the_printing_and_typesetting_industry__lorem_ipsum_has_been_the_industry_s_standard_dummy_;
+	private ImageView icon_ionic_ios_heart_empty_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.first_screen___2);
+
+		
+		_bg__first_screen___2 = (View) findViewById(R.id._bg__first_screen___2);
+		path_1 = (ImageView) findViewById(R.id.path_1);
+		ellipse_2 = (View) findViewById(R.id.ellipse_2);
+		ellipse_3 = (View) findViewById(R.id.ellipse_3);
+		ellipse_4 = (View) findViewById(R.id.ellipse_4);
+		icon_ionic_ios_arrow_back = (ImageView) findViewById(R.id.icon_ionic_ios_arrow_back);
+		full_size = (View) findViewById(R.id.full_size);
+		full_size_ek1 = (View) findViewById(R.id.full_size_ek1);
+		icon_ionic_ios_heart_empty = (ImageView) findViewById(R.id.icon_ionic_ios_heart_empty);
+		shopping_cart = (ImageView) findViewById(R.id.shopping_cart);
+		home = (ImageView) findViewById(R.id.home);
+		full_size_ek2 = (ImageView) findViewById(R.id.full_size_ek2);
+		mask_group_1 = (ImageView) findViewById(R.id.mask_group_1);
+		details = (TextView) findViewById(R.id.details);
+		reviews = (TextView) findViewById(R.id.reviews);
+		family = (TextView) findViewById(R.id.family);
+		size = (TextView) findViewById(R.id.size);
+		cetagory = (TextView) findViewById(R.id.cetagory);
+		gimbarg = (TextView) findViewById(R.id.gimbarg);
+		height__62_inc = (TextView) findViewById(R.id.height__62_inc);
+		indor = (TextView) findViewById(R.id.indor);
+		icon_awesome_star = (ImageView) findViewById(R.id.icon_awesome_star);
+		icon_awesome_star_ek1 = (ImageView) findViewById(R.id.icon_awesome_star_ek1);
+		icon_awesome_star_ek2 = (ImageView) findViewById(R.id.icon_awesome_star_ek2);
+		icon_awesome_star_ek3 = (ImageView) findViewById(R.id.icon_awesome_star_ek3);
+		icon_awesome_star_ek4 = (ImageView) findViewById(R.id.icon_awesome_star_ek4);
+		details_ek1 = (TextView) findViewById(R.id.details_ek1);
+		ellipse_7 = (View) findViewById(R.id.ellipse_7);
+		_3606 = (ImageView) findViewById(R.id._3606);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		price___200 = (TextView) findViewById(R.id.price___200);
+		buy_it_now = (TextView) findViewById(R.id.buy_it_now);
+		lorem_ipsum_is_simply_dummy_text_of_the_printing_and_typesetting_industry__lorem_ipsum_has_been_the_industry_s_standard_dummy_ = (TextView) findViewById(R.id.lorem_ipsum_is_simply_dummy_text_of_the_printing_and_typesetting_industry__lorem_ipsum_has_been_the_industry_s_standard_dummy_);
+		icon_ionic_ios_heart_empty_ek1 = (ImageView) findViewById(R.id.icon_ionic_ios_heart_empty_ek1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
